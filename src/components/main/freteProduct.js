11f6867ff4f1e53db19c81b03dfa615c1cf732df
tiagolucasoo <tiago.lucas.oliveira18@gmail.com>
2025-11-07 10:33:25 -0300

    Feat: Ajustes produtos e frete

--- src/components/main/freteProduct.js
+++ src/components/main/freteProduct.js
@@ -5,8 +5,27 @@ function FreteProduct() {
     <div className="FreteProduct">
       <main className="FreteProduct-main">
         <h3>Calcular Frete.:</h3>
-        <input placeholder='Digite aqui o cep'></input>
-        <button className="frete">Consultar</button>
+
+        <div className="Subcontainer">
+          <input placeholder='Digite aqui o cep'></input>
+          <button className="frete">Consultar</button>
+        </div>
+        <div className="InfoFrete">
+          <div className="Fretecidade">
+            <strong>Cidade</strong>
+            <p className='TextCidade'>Londrina - PR</p>
+          </div>
+
+          <div className="Fretecusto">
+            <strong>Custo</strong>
+            <p className='TextCusto'>R$10,00</p>
+          </div>
+
+          <div className="Freteprazo">
+            <strong>Prazo</strong>
+            <p className='TextPrazo'>3 à 5 dias úteis</p>
+          </div>
+        </div>
       </main>
     </div>
   );
